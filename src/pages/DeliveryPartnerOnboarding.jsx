0339bc79af9adb552dc09d02ffa407880c7c40a7
dharti1812 <71380470+dharti1812@users.noreply.github.com>
2025-12-13 18:35:07 +0530

    update step

--- src/pages/DeliveryPartnerOnboarding.jsx
+++ src/pages/DeliveryPartnerOnboarding.jsx
@@ -211,7 +211,7 @@ export default function DeliveryPartnerOnboarding() {
           formData.phone,
           "delivery_boy"
         );
-        setStep(result.data.deliveryboy_onboarding_step);
+        setStep(result.deliveryboy_onboarding_step);
       } else {
         setError("❌ " + (result.message || "Invalid OTP"));
       }
