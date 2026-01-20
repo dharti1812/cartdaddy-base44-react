ceadaed6bb0c23abb50d3bc3c037a9ba6fe6bf67
dharti1812 <71380470+dharti1812@users.noreply.github.com>
2026-01-20 17:44:05 +0530

    update date

--- src/pages/DeliveryBoyPortal.jsx
+++ src/pages/DeliveryBoyPortal.jsx
@@ -827,7 +827,7 @@ export default function DeliveryBoyPortal() {
                             <div className="flex justify-between items-center">
                               <h3 className="font-bold text-lg text-gray-900">
                                 {order.website_ref ||
-                                  `Order #${String(order.id)}`}  ({order.created_at})
+                                  `Order #${String(order.id)}`}  ({order.created_date})
                               </h3>
                               <span className=" py-1 text-xs font-semibold bg-green-100 text-green-700 rounded-full capitalize">
                                 {order.payment_status}
@@ -1104,7 +1104,7 @@ export default function DeliveryBoyPortal() {
                         >
                           <CardContent className="p-4">
                             <h3 className="font-bold text-lg mb-2">
-                              {order.website_ref || `Order #${order.id}`}  ({order.created_at})
+                              {order.website_ref || `Order #${order.id}`}  ({order.created_date})
                             </h3>
 
                             <p className="text-gray-700 text-sm flex items-center gap-2">
